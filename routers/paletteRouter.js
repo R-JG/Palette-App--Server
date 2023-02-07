f14aec1eb56f7d199bc40c508baeb2a2907acb54
Robin <robin.james.guerrier@gmail.com>
2023-02-06 20:57:16 -0500

    Add post to /api/palette route handler which saves a new palette with an optionally defined name and an empty colors array.

--- routers/paletteRouter.js
+++ routers/paletteRouter.js
@@ -1,11 +1,25 @@
 const paletteRouter = require('express').Router();
 const Palette = require('../models/palette');
 
+
 paletteRouter.get('/', (request, response, next) => {
     Palette
         .find({})
         .then(allPalettes => response.json(allPalettes))
         .catch(error => next(error));
 });
+
+paletteRouter.post('/', (request, response, next) => {
+    const body = request.body;
+    const newPalette = new Palette({
+        name: body.name || 'New Palette',
+        colors: []
+    });
+    newPalette
+        .save()
+        .then(savedPalette => response.json(savedPalette))
+        .catch(error => next(error));
+});
+
 
 module.exports = paletteRouter;
